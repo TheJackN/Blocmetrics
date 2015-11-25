e7a2251982db958288ce5b7b4d9b2f2405f05014
James <JamesCarlson2@hotmail.com>
2015-11-25 10:59:06 -0600

    Incorporate Devise, Pundit, Simple_Form, RSpec, Factorygirl, Faker, Bootstrap, Bootstrap-sprockets and Figaro. Create User model and controller. Set basic application layout styles. Configure routes. Use Home controller for root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
+
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  private
+  def user_not_authorized
+    flash[:error] = "You do not have permission to do that"
+    redirect_to(request.referrer || root_path)
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
